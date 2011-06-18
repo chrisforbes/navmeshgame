2b2ef4b593697fddf176e2c2ba08d6100e88c57a
Chris Forbes <chrisf@ijw.co.nz>
2011-06-18 18:40:18 +1200

    poly attrib colors

--- level.py
+++ level.py
@@ -1,16 +1,16 @@
 from pygame.draw import polygon
 
-walkable_color = 0, 0, 0
-
 class Level:
 
     def __init__( self, screen ):
         self.screen = screen
         self.verts = [ (200,200), (400,200), (400,400), (200,400) ]
         self.polys = [ [ 0, 1, 2, 3 ] ]
+        # 0 = walkable, 1 = shootable
+        self.colors = [ (0, 0, 0), (40, 40, 40) ]
 
     def draw( self ):
         for poly in self.polys:
             actual_verts = [ self.verts[x] for x in poly ]
-            polygon( self.screen, walkable_color, actual_verts )
+            polygon( self.screen, self.colors[0], actual_verts )
 
