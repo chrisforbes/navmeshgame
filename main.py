df8b3c4828278e1d51e5a44d26e3ce6f2acfb667
Chris Forbes <chrisf@ijw.co.nz>
2011-06-18 17:47:58 +1200

    add selectability

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ from math import sin, cos
 
 background_color = 80, 80, 80
 dude_color = 160, 0, 0
+highlight_color = 140, 80, 80
 screen = None
 
-def draw_dude(x, y, angle):
+def draw_dude(x, y, angle, selected):
 	pointer = (x + 15 * cos(angle), y + 15 * sin(angle))
-	
+
+	if selected:
+		circle( screen, highlight_color, (x, y), 10, 3 )
+		line( screen, highlight_color, (x, y), pointer, 3 )
+
 	circle( screen, dude_color, (x, y), 10, 1 )
 	line( screen, dude_color, (x, y), pointer, 1 )
 
@@ -30,7 +35,7 @@ def main():
 
 		x = x + 1
 		if x > 100: x = 0
-		draw_dude( 300 + x, 300, 0.3 )
+		draw_dude( 300 + x, 300, 0.3, True )
 
 		pygame.display.flip()
 
