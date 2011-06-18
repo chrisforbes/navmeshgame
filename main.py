219e47354195b281b719c5521277648d0f397d49
Chris Forbes <chrisf@ijw.co.nz>
2011-06-18 17:56:05 +1200

    render level geometry

--- main.py
+++ main.py
@@ -2,16 +2,22 @@
 
 import pygame
 from pygame.locals import QUIT
-from pygame.draw import circle, line
+from pygame.draw import circle, line, polygon
 from math import sin, cos
 
 background_color = 80, 80, 80
 dude_color = 160, 0, 0
 highlight_color = 140, 80, 80
+walkable_color = 0, 0, 0
 screen = None
 
 level_verts = [ (200,200), (400,200), (400,400), (200,400) ]
-level_polys = [ 0, 1, 2, 3 ]
+level_polys = [ [ 0, 1, 2, 3 ] ]
+
+def draw_level():
+	for poly in level_polys:
+		actual_verts = [ level_verts[x] for x in poly ]
+		polygon( screen, walkable_color, actual_verts )
 
 def draw_dude(x, y, angle, selected):
 	pointer = (x + 15 * cos(angle), y + 15 * sin(angle))
@@ -36,8 +42,12 @@ def main():
 
 		screen.fill( background_color )
 
+		# animate
 		x = x + 1
 		if x > 100: x = 0
+
+		# draw
+		draw_level()
 		draw_dude( 300 + x, 300, 0.3, True )
 
 		pygame.display.flip()
