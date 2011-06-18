5916125e82c67df320c239d61474153e2a83f7bc
Chris Forbes <chrisf@ijw.co.nz>
2011-06-18 18:38:09 +1200

    verts,polys as instance members

--- level.py
+++ level.py
@@ -2,15 +2,15 @@ from pygame.draw import polygon
 
 walkable_color = 0, 0, 0
 
-level_verts = [ (200,200), (400,200), (400,400), (200,400) ]
-level_polys = [ [ 0, 1, 2, 3 ] ]
-
 class Level:
+
     def __init__( self, screen ):
         self.screen = screen
+        self.verts = [ (200,200), (400,200), (400,400), (200,400) ]
+        self.polys = [ [ 0, 1, 2, 3 ] ]
 
     def draw( self ):
-        for poly in level_polys:
-            actual_verts = [ level_verts[x] for x in poly ]
+        for poly in self.polys:
+            actual_verts = [ self.verts[x] for x in poly ]
             polygon( self.screen, walkable_color, actual_verts )
 
