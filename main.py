6fec460c0c9925bedb597cf496ae87ef524f77ea
Chris Forbes <chrisf@ijw.co.nz>
2011-06-18 17:17:14 +1200

    blah

--- main.py
+++ main.py
@@ -3,14 +3,17 @@
 import pygame
 from pygame.locals import *
 
+black = 0, 0, 0
+
 def main():
 	pygame.init()
-	pygame.display.set_mode((800,600))
+	screen = pygame.display.set_mode((800, 600))
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				return
+		screen.fill( black )
 		pygame.display.flip()
 
 if __name__ == '__main__':
